b66860b77eea08dd1e27881835d6eba23f884bfd
Rachele Edwards <dot.com1472@gmail.com>
2020-03-24 14:47:50 -0600

    started to create form on Form.js--shows up on the app after fixing all syntax errors. Moving on

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -5,22 +5,36 @@ import Form from "./childFolder/Form"
 
 //set up inital team for testing
 const initialTeam = [
-  {id: uuid(), first: 'Austin', last: 'Lynes'},
+  {id: uuid(), first: 'Austin', last: 'Lynes', role: 'Team Lead', email: 'austinlynes@lambdaschool.com'},
 ]
 
 function App() {
   //set up a state property for your team members list
   const [teams, setTeams] = useState(initialTeam)
 
+  //Give the state variable you just declared a default value. 
+  //You will need to keep track of a list of team members and each 
+  //team member will have several key/value pairs associated with them.
+  const [formValues, setFormValues] = useState({
+    first: '',
+    last: '',
+    role: '',
+    email: '',
+  })
 
   return (
     <div className="App">
-     <h1>Team Austin</h1>
-     <h3>Meet your team:</h3>
-     {
-     teams.map(team =>
-       <div key={team.id}>{team.first} {team.last}</div>)
-     }
+      <h1>Team Austin</h1>
+      <h3>Meet your team:</h3>
+      {teams.map(team =>
+        <div key={team.id}><b>{team.first} {team.last}:</b> <br /> Role: {team.role} <br /> Email: {team.email}</div>)
+      }
+      <br/><br/>
+      <h4>Add a New Member:</h4>
+      <Form>
+        
+        formValues={formValues}
+      </Form>
     </div>
   );
 }
--- team-builder/src/childFolder/Form.js
+++ team-builder/src/childFolder/Form.js
@@ -1,9 +1,37 @@
 import React from "react"
 
-const Form = () => {
-    
+const Form = props => {
     return (
-        console.log("Hello from Form.js")
+        // console.log("Hello from Form.js")
+        <form>
+            <label>Enter First Name:
+                <input className="first-name"
+                    value={props.first}
+                    name='first'
+                />
+            </label>
+
+            <label>Enter Last Name:
+                <input className="last-name"
+                    value={props.last}
+                    name='last'
+                />
+            </label>
+
+            <label>Role: 
+                <input className="role"
+                    value={props.role}
+                    name='role'
+                />
+            </label>
+
+            <label>Email: 
+                <input className="email"
+                    value={props.email}
+                    name='email'
+                />
+            </label>
+        </form>
     )
 }
 
